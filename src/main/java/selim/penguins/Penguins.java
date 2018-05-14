5d96b56479908f04b9846b383134883c699d61f6
Myles Selim <Selim042@users.noreply.github.com>
2018-05-14 14:38:24 -0500

    Version bump

--- src/main/java/selim/penguins/Penguins.java
+++ src/main/java/selim/penguins/Penguins.java
@@ -61,7 +61,7 @@ public class Penguins {
 
 	public static final String MODID = "selimpenguin";
 	public static final String NAME = "Penguin";
-	public static final String VERSION = "1.1.1";
+	public static final String VERSION = "1.2.0";
 	@Mod.Instance(value = MODID)
 	public static Penguins instance;
 	public static final Logger LOGGER = LogManager.getLogger(MODID);
