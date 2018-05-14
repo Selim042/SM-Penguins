d680dd21bb218ea0b5b170c7a9f395c1fb01bc19
Myles Selim <Selim042@users.noreply.github.com>
2018-05-14 12:24:13 -0500

    Forgot this one

--- src/main/java/selim/penguins/penguin/PenguinRenderer.java
+++ src/main/java/selim/penguins/penguin/PenguinRenderer.java
@@ -4,9 +4,6 @@ import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.util.ResourceLocation;
 import selim.penguins.Penguins;
-import selim.penguins.apparel.ModelPenguinBowtie;
-import selim.penguins.apparel.ModelPenguinScarf;
-import selim.penguins.apparel.ModelPenguinTophat;
 import selim.penguins.layers.LayerApparelColored;
 import selim.penguins.layers.LayerApparelPatterned;
 
@@ -14,19 +11,8 @@ public class PenguinRenderer extends RenderLiving<EntityPenguin> {
 
 	public PenguinRenderer(RenderManager manager) {
 		super(manager, new ModelPenguin(), 0.35f);
-		this.layerRenderers
-				.add(new LayerApparelColored(this, new ModelPenguinBowtie(), Penguins.Items.BOWTIE,
-						new ResourceLocation(Penguins.MODID, "textures/apparel/bowtie.png")));
-		this.layerRenderers
-				.add(new LayerApparelColored(this, new ModelPenguinTophat(), Penguins.Items.TOPHAT,
-						new ResourceLocation(Penguins.MODID, "textures/apparel/tophat.png"),
-						new ResourceLocation(Penguins.MODID, "textures/apparel/tophat_stripe.png")));
-		this.layerRenderers
-				.add(new LayerApparelPatterned(this, new ModelPenguinScarf(), Penguins.Items.SCARF,
-						new ResourceLocation(Penguins.MODID, "textures/apparel/scarf.png")));
-		// this.layerRenderers.add(new LayerApparel(this, new ModelPenguinFez(),
-		// Penguins.Items.FEZ,
-		// new ResourceLocation(Penguins.MODID, "textures/apparel/fez.png")));
+		this.layerRenderers.add(new LayerApparelColored(this));
+		this.layerRenderers.add(new LayerApparelPatterned(this));
 	}
 
 	private static final ResourceLocation texture = new ResourceLocation(
